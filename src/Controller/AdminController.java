eb81f85a8f17ebd07689e06becda58de85b6d5c9
Ariel Valverde Ramirez <38445003+arielvalverde@users.noreply.github.com>
2020-08-02 19:33:57 -0600

    Reportes primera parte

--- src/Controller/AdminController.java
+++ src/Controller/AdminController.java
@@ -9,7 +9,10 @@ import Model.Admin.Employee;
 import Model.Admin.Establishment;
 import Model.Admin.FurnitureStore;
 import Model.Admin.WorkShop;
+import Model.Client.Order;
+import Model.Products.Product;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
@@ -40,4 +43,98 @@ public class AdminController {
         employees.add(empl);
         return empl;
     }
+    
+    public void reportingSalesFurniture(Employee e){
+        int total = 0;
+        for(Order o: MainController.getInstance().getClientController().getOrders()){
+            if(o.getShop().equals(e.getStore())){
+                System.out.print("Esta Perro");
+                System.out.print(o.getClient().getName());
+                System.out.print(o.getCost());
+                total = total + o.getCost();
+                System.out.print(o.getDate());
+                System.out.print(o.getProducts());
+            }
+        }
+        System.out.print("Total: "+total);
+    }
+    
+    public void reportingSalesGeneral(){
+        int total = 0;
+        for(Order o: MainController.getInstance().getClientController().getOrders()){
+            System.out.print(o.getClient().getName());
+            System.out.print(o.getCost());
+            total = total + o.getCost();
+            System.out.print(o.getDate());
+            System.out.print(o.getProducts());
+        }
+        System.out.print("Total: "+total);
+    }
+    
+    public void profit(FurnitureStore fstore, Date dateI, Date dateF, Product product,int expenses){
+        int total = 0;
+        for(Order o: MainController.getInstance().getClientController().getOrders()){
+            if(fstore.equals(null)){
+                if(dateI == null && dateF==null){
+                    if(product==null){
+                        total=total+o.getCost();
+                    }
+                    else{
+                        for(Product p:o.getProducts()){
+                            if(p.equals(product)){
+                                total=total+p.getCost();
+                            }
+                        }
+                    }
+                }
+                else{
+                    if(dateI.before(o.getDate()) && dateF.after(o.getDate())){
+                        if(product==null){
+                            total=total+o.getCost();
+                        }
+                        else{
+                            for(Product p:o.getProducts()){
+                                if(p.equals(product)){
+                                    total=total+p.getCost();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else{
+                if(o.getShop().equals(fstore)){
+                    if(dateI == null && dateF==null){
+                        if(product==null){
+                            total=total+o.getCost();
+                        }
+                        else{
+                            for(Product p:o.getProducts()){
+                                if(p.equals(product)){
+                                    total=total+p.getCost();
+                                }
+                            }
+                        }
+                    }
+                    else{
+                        if(dateI.before(o.getDate()) && dateF.after(o.getDate())){
+                            if(product==null){
+                                total=total+o.getCost();
+                            }
+                            else{
+                                for(Product p:o.getProducts()){
+                                    if(p.equals(product)){
+                                        total=total+p.getCost();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        total=total-expenses;
+        System.out.print(total);
+    }
+    
 }
--- src/Model/Client/Order.java
+++ src/Model/Client/Order.java
@@ -33,6 +33,61 @@ public class Order {
         this.shop = employee.getStore();
         this.client = client;
     }
-        
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public FurnitureStore getShop() {
+        return shop;
+    }
+
+    public void setShop(FurnitureStore shop) {
+        this.shop = shop;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isDeliveryRequired() {
+        return deliveryRequired;
+    }
+
+    public void setDeliveryRequired(boolean deliveryRequired) {
+        this.deliveryRequired = deliveryRequired;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
         
 }
